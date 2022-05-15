2cc33e8baaca243143d5ecbbe71a2d3f9c5eef1d
Karl Gluck <karlgluck@users.noreply.github.com>
2022-05-14 17:27:12 -0700

    require administrator privileges

--- Module/Host/Update-SwissHost.ps1
+++ Module/Host/Update-SwissHost.ps1
@@ -12,6 +12,13 @@ function Update-SwissHost {
     $Bootstrap,
     [Switch]$AtStartup
   )
+
+  # Precondition: Administrator privileges
+  if (-not (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)))
+  {
+      Write-Host -ForegroundColor Red "Must run in an Administrator PowerShell terminal (open with Win+X, A)"
+      return
+  }
   
   if ($Bootstrap -ne $null)
   {
