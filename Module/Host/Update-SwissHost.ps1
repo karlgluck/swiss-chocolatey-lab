277b1c496087cac0769b05632cdfe9a871889100
Karl Gluck <karlgluck@users.noreply.github.com>
2022-05-15 19:39:42 -0700

    fix up other names missing "-Lab" at the end

--- Module/Host/Update-SwissHost.ps1
+++ Module/Host/Update-SwissHost.ps1
@@ -271,8 +271,8 @@ function Update-SwissHost {
   Install-Module AutomatedLab -AllowClobber
   $LabSourcesFolder = New-LabSourcesFolder -Drive $Config.AutomatedLab.SourcesFolderDrive
   Write-Host "AutomatedLab ready in $LabSourcesFolder"
-  $ToolsPath = Join-Path $LabSourcesFolder "Tools/SwissChocolatey"
-  $PostInstallationActivitiesPath = Join-Path $LabSourcesFolder "PostInstallationActivities/SwissChocolatey"
+  $ToolsPath = Join-Path $LabSourcesFolder "Tools/SwissChocolateyLab"
+  $PostInstallationActivitiesPath = Join-Path $LabSourcesFolder "PostInstallationActivities/SwissChocolateyLab"
 
 
 
