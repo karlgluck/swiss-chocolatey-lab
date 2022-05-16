8100d8137c340a589ffe85928d56d92902a168c1
Karl Gluck <karlgluck@users.noreply.github.com>
2022-05-15 19:21:00 -0700

    Create Update-SwissGuest.ps1

---
+++ Module/Guest/Update-SwissGuest.ps1
@@ -0,0 +1,12 @@
+<#
+#>
+function Update-SwissGuest {
+  Param ()
+  
+  # Download the latest copy of the SwissChocolateyLab repository
+  
+  # Copy the SCL module into our PowerShell modules directory
+  
+  # Get the latest copy of the repository
+  
+}
