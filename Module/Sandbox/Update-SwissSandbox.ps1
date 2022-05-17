1927d20b815a97bc8dbf0d05ea8bf5684a3ebc0f
Karl Gluck <karlgluck@users.noreply.github.com>
2022-05-16 23:13:09 -0700

    sandbox bootstrap, install choco packages

--- Module/Sandbox/Update-SwissSandbox.ps1
+++ Module/Sandbox/Update-SwissSandbox.ps1
@@ -11,6 +11,7 @@ function Update-SwissSandbox {
   )
 
 
+
   # Load the guest config file
   $GuestConfigPath = Join-Path ([Environment]::GetFolderPath("MyDocuments")) ".swissguest"
   if (Test-Path $GuestConfigPath)
@@ -24,6 +25,110 @@ function Update-SwissSandbox {
     return
   }
 
+
+
+  # Get the repository's configuration file
+  if ($null -ne $Bootstrap)
+  {
+    Write-Host "Bootstrapping sandbox '${env:ComputerName}'"
+
+    $HostConfig = [PSCustomObject]@{}
+
+    # Save the access token
+    Add-Member -Name 'Token' -Value $Bootstrap.Token -Force -InputObject $HostConfig -MemberType NoteProperty
+
+    # Parse the host's repository configuration from HostUrl
+    $Match = [RegEx]::Match($Bootstrap.HostUrl, 'githubusercontent\.com\/([^\/]+)\/([^\/]+)\/(\S+)\/Module\/Sandbox\/Update-SwissSandbox.ps1')
+    if ($Match.Success)
+    {
+      Add-Member -Name 'Username' -Value $Match.Groups[1].Value -Force -InputObject $HostConfig -MemberType NoteProperty
+      Add-Member -Name 'Repository' -Value $Match.Groups[2].Value -Force -InputObject $HostConfig -MemberType NoteProperty
+      Add-Member -Name 'Branch' -Value $Match.Groups[3].Value -Force -InputObject $HostConfig -MemberType NoteProperty
+      Add-Member -Name 'RawUrl' -Value "https://raw.githubusercontent.com/$($HostConfig.UserName)/$($HostConfig.Repository)/$($HostConfig.Branch)" -Force -InputObject $HostConfig -MemberType NoteProperty
+      Add-Member -Name 'ZipUrl' -Value "https://github.com/$($HostConfig.UserName)/$($HostConfig.Repository)/archive/refs/heads/$($HostConfig.Branch).zip" -Force -InputObject $HostConfig -MemberType NoteProperty
+    }
+    else
+    {
+      Write-Host -ForegroundColor Red ">>>> Bootstrapping URL doesn't match expected format (see README.md) <<<<"
+      return
+    }
+
+    # Save the host config
+    Add-Member -Name 'HostConfig' -Value $HostConfig -Force -InputObject $GuestConfig -MemberType NoteProperty
+
+    # Parse the guest's repository configuration from GuestUrl
+    $Match = [RegEx]::Match($Bootstrap.GuestUrl, 'github\.com\/([^\/]+)\/([^\/]+)\/(\S+)\/?')
+    if ($Match.Success)
+    {
+      Add-Member -Name 'Username' -Value $Match.Groups[1].Value -Force -InputObject $GuestConfig -MemberType NoteProperty
+      Add-Member -Name 'Repository' -Value $Match.Groups[2].Value -Force -InputObject $GuestConfig -MemberType NoteProperty
+      Add-Member -Name 'Branch' -Value $Match.Groups[3].Value -Force -InputObject $GuestConfig -MemberType NoteProperty
+      Add-Member -Name 'RawUrl' -Value "https://raw.githubusercontent.com/$($GuestConfig.UserName)/$($GuestConfig.Repository)/$($GuestConfig.Branch)" -Force -InputObject $GuestConfig -MemberType NoteProperty
+      Add-Member -Name 'ZipUrl' -Value "https://github.com/$($GuestConfig.UserName)/$($GuestConfig.Repository)/archive/refs/heads/$($GuestConfig.Branch).zip" -Force -InputObject $GuestConfig -MemberType NoteProperty
+    }
+    else
+    {
+      Write-Host -ForegroundColor Red ">>>> Guest repository URL doesn't match expected format (see README.md) <<<<"
+      return
+    }
+
+    # Prevent accidental use of the Bootstrap variable later in the script
+    Remove-Variable -Name 'Bootstrap'
+  }
+  else
+  {
+    # Expect a config file to exist, otherwise we can't know what to do
+    if (-not (Test-Path $ConfigPath))
+    {
+      Write-Host -ForegroundColor Red ">>>> FATAL: Missing config file. Try bootstrapping again? (see README.md) <<<<"
+      Write-Host -ForegroundColor Red "Expected: $ConfigPath"
+      return
+    }
+  }
+
+
+
+  # Grab the configuration from the repository and merge it into $Config
+  $RemoteConfig = [PSCustomObject]@{}
+  try
+  {
+    $RemoteConfig = (Invoke-WebRequest -Method Get -Uri $HostSpecificConfigUrl -Headers $Headers).Content | ConvertFrom-Json
+    Write-Host "Using host-specific config: $HostSpecificConfigUrl"
+  }
+  catch
+  {
+    try
+    {
+      $RemoteConfig = (Invoke-WebRequest -Method Get -Uri $GenericConfigUrl -Headers $Headers).Content | ConvertFrom-Json
+      Write-Host "Using generic host config: $GenericConfigUrl"
+    }
+    catch
+    {
+      Write-Host -ForegroundColor Red ">>>> Missing both default and host-specific config files in repository <<<<"
+      Write-Host -ForegroundColor Red "Expected either:"
+      Write-Host -ForegroundColor Red " * $GenericConfigUrl"
+      Write-Host -ForegroundColor Red " * $HostSpecificConfigUrl"
+      return;
+    }
+  }
+  finally
+  {
+    if (Test-Path 'variable:RemoteConfig')
+    {
+      # Move properties into Config
+      $RemoteConfig.PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" } | ForEach-Object { Add-Member -Name $_.Name -Value $_.Value -Force -InputObject $Config -MemberType NoteProperty }
+      Remove-Variable -Name RemoteConfig
+    }
+  }
+
+
+
+  # Write the configuration file
+  ConvertTo-Json $RemoteConfig | Out-File -FilePath $GuestConfigPath
+
+
+
+
   # Derived variables
   $GuestHeaders = @{Authorization=('token ' + $GuestConfig.Token); 'Cache-Control'='no-store'}
   $ModulesFolder = Join-Path ($env:PSModulePath -split ';')[0] "SwissChocolateyLab"
@@ -34,7 +139,7 @@ function Update-SwissSandbox {
 
 
   # Download the latest copy of the host repository
-  $TempRepositoryZipPath = Join-Path ([System.IO.Path]::GetTempPath()) "$($GuestConfig.Username)-$($GuestConfig.Repository)-$([System.IO.Path]::GetRandomFileName()).zip"
+  $TempRepositoryZipPath = Join-Path ([System.IO.Path]::GetTempPath()) "$($GuestConfig.UserName)-$($GuestConfig.Repository)-$([System.IO.Path]::GetRandomFileName()).zip"
   try
   {
     Write-Host "Downloading latest '$($GuestConfig.HostConfig.Repository)' branch $($GuestConfig.HostConfig.Branch) -> $TempRepositoryZipPath"
@@ -50,7 +155,8 @@ function Update-SwissSandbox {
 
 
 
-  # Install /Module/** as a PowerShell module
+
+  # Install <host-repo>/Module/** as a PowerShell module
   # https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/extract-specific-files-from-zip-archive
   Write-Host "Install <repo>/Module/** as a PowerShell module into $ModulesFolder"
   if (Test-Path $ModulesFolder) { Remove-Item -Recurse -Force $ModulesFolder }
@@ -90,6 +196,61 @@ function Update-SwissSandbox {
     choco feature enable -n=allowGlobalConfirmation --no-color | Out-Null
     choco feature enable -n=exitOnRebootDetected --no-color | Out-Null
   }
+
+
+
+
+  # Download <guest-repo>/.swiss/packages.config
+  try
+  {
+    Invoke-WebRequest -Method Get -Uri $PackagesConfigUrl -Headers $GuestHeaders -OutFile $PackagesConfigPath
+    Write-Host "Installing choco packages from $PackagesConfigUrl"
+  }
+  catch
+  {
+    if (-not (Test-Path $PackagesConfigPath))
+    {
+      Write-Host -ForegroundColor Yellow @"
+Missing Chocolatey configuration. No packages will be installed. Expecting either:
+    * $PackagesConfigUrl
+    * $PackagesConfigPath
+"@
+    }
+  }
+
+
+
+
+  # Install packages.config using Chocolatey
+  if (Test-Path $PackagesConfigPath)
+  {
+    $packagesAlreadyInstalled = (choco list --limit-output --local-only) | ForEach-Object { $_.Split("|")[0] }
+    $packagesRequiredByConfig = Select-Xml -Path $PackagesConfigPath -XPath "packages/package" | ForEach-Object { $_.Node.id }
+    $packagesToInstall = Compare-Object -ReferenceObject $packagesAlreadyInstalled -DifferenceObject $packagesRequiredByConfig | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object { $_.InputObject }
+
+    if ($packagesToInstall.Count -gt 0)
+    {
+      [void](& choco install $PackagesConfigPath --limit-output --no-color -y)
+      $chocoExitCode = $LASTEXITCODE
+
+      $packagesSubsequentlyInstalled = (choco list --limit-output --local-only) | ForEach-Object { $_.Split("|")[0] }
+      $packagesNotInstalled = Compare-Object -ReferenceObject $packagesSubsequentlyInstalled -DifferenceObject $packagesRequiredByConfig | Where-Object { $_.SideIndicator -eq "=>" } | ForEach-Object { $_.InputObject }
+      if ($packagesNotInstalled.Count -gt 0)
+      {
+        Write-Host -ForegroundColor Yellow "Not all packages could be installed. Missing: $($packagesNotInstalled -join ', ')"
+        $chocoExitCode = 9999
+      }
+      if (@(350, 1604, 1614, 1641, 3010, 9999) -contains $chocoExitCode)
+      {
+        Write-Host -ForegroundColor Yellow "Chocolatey exited with $chocoExitCode. Restart your computer to continue installing packages"
+        if ($GuestConfig.AutoUpdateEnabled)
+        {
+          Restart-Computer
+        }
+      }
+    }
+  }
+
 
 
 }
