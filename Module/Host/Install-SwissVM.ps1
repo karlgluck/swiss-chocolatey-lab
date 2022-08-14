34f7923e335c86bb1b5eb8f0b6b3ec8fc81284b8
Karl Gluck <karlgluck@users.noreply.github.com>
2022-08-14 16:29:12 -0700

    more robust available drive path check

--- Module/Host/Install-SwissVM.ps1
+++ Module/Host/Install-SwissVM.ps1
@@ -195,7 +195,7 @@ function Install-SwissVM {
 
 
   # Map a network drive to the VM's Shared folder
-  $AvailableDrivePath = (Compare-Object (Get-PSDrive | % { $_.Name }) (67..90 | %{[char]$_}) | Where-Object { $_.SideIndicator -eq "=>" } | Select-Object -First 1).InputObject
+  $AvailableDrivePath = (Compare-Object (((Get-PSDrive | % { $_.Name }) + (Get-WmiObject Win32_NetworkConnection | % { $_.LocalName.Substring(0,1) }))) (67..90 | %{[char]$_}) | Where-Object { $_.SideIndicator -eq "=>" } | Select-Object -First 1).InputObject
   $Domain = $VMName.ToUpper()
   $Username = "$Domain\$($GuestConfig.Username)"
   $Password = ConvertTo-SecureString $Repository -AsPlainText -Force
